f9fd4dd12e4e0ec7ad2b49a3012b513226fbe964
Ali Jan <alijaanm110@gmail.com>
2019-07-01 21:03:24 +0300

    added xlsx file, 10K Fold accuracy scores

--- main.py
+++ main.py
@@ -37,38 +37,28 @@ def logisticRegression(X_train_std, y_train, combinedX, combinedY):
     lrClf = LogisticRegression()
     lrClf.fit(X_train_std, y_train)
     prob = lrClf.predict_proba(X_test_std)
-    kFold = round(sum(cross_val_score(lrClf, combinedX, combinedY, cv=10, scoring='f1_micro'))*10.0,2)
-    return kFold, prob
+    kFold = round(sum(cross_val_score(lrClf, combinedX, combinedY, cv=10, scoring='f1_micro')) * 10.0, 2)
+    return kFold, prob, lrClf
 
 
-def svm(X_train_std, y_train):
+def svm(X_train_std, y_train, combinedX, combinedY):
     svmClf = SVC()
     svmClf.fit(X_train_std, y_train)
-    scores = []
-    cv = KFold(n_splits=10, random_state=42, shuffle=True)
-    for train_index, test_index in cv.split(X_train_std):
-        svmClf.fit(X_train, y_train)
-        scores.append(svmClf.score(X_test, y_test))
-    kFold = cross_val_score(svmClf, X_train_std, y_train, cv=10)
+    kFold = round(sum(cross_val_score(svmClf, combinedX, combinedY, cv=10, scoring='f1_micro')) * 10.0, 2)
     return kFold
 
 
-def rfClassifier(X_train_std, y_train):
+def rfClassifier(X_train_std, y_train, combinedX, combinedY):
     rfClf = RandomForestClassifier()
     rfClf.fit(X_train_std, y_train)
-    scores = []
-    cv = KFold(n_splits=10, random_state=42, shuffle=True)
-    for train_index, test_index in cv.split(X_train_std):
-        rfClf.fit(X_train, y_train)
-        scores.append(rfClf.score(X_test, y_test))
-    kFold = cross_val_score(rfClf, X_train_std, y_train, cv=10)
+    kFold = round(sum(cross_val_score(rfClf, combinedX, combinedY, cv=10, scoring='f1_micro')) * 10.0, 2)
     return kFold
 
 
 def dtClassifier(X_train_std, y_train, combinedX, combinedY):
     dtClf = DecisionTreeClassifier()
     dtClf.fit(X_train_std, y_train)
-    kFold = round(sum(cross_val_score(dtClf, combinedX, combinedY, cv=10, scoring='f1_micro'))*10.0,2)
+    kFold = round(sum(cross_val_score(dtClf, combinedX, combinedY, cv=10, scoring='f1_micro')) * 10.0, 2)
     return kFold
 
 
@@ -77,9 +67,10 @@ def plotAccuracy(acc1, acc2, acc3, acc4):
     index = np.arange(len(labels))
     plotter = plt.bar(index, [acc1, acc2, acc3, acc4])
     plotter[0].set_color('r'), plotter[1].set_color('b'), plotter[2].set_color('pink'), plotter[3].set_color('y')
-    plt.xlabel('Classifiers', fontsize=8)
-    plt.ylabel('Classifiers\'s Scores', fontsize=8)
+    plt.xlabel('Classifiers', fontsize=10)
+    plt.ylabel('Classifiers\'s Scores', fontsize=10)
     plt.xticks(index, labels, fontsize=10, rotation=30)
+    plt.ylim((0, 100))
     plt.title('10-K Cross-Validation')
     plt.show()
 
@@ -89,12 +80,10 @@ X_train_std, X_test_std = scaleFeatures(X_train, X_test)
 combinedX = np.vstack((X_train_std, X_test_std))
 combinedY = np.hstack((y_train, y_test))
 
-lrAccuracy, prob = logisticRegression(X_train_std, y_train, combinedX, combinedY)
+lrAccuracy, prob , lrClf= logisticRegression(X_train_std, y_train, combinedX, combinedY)
+svmAccuracy = svm(X_train_std, y_train, combinedX, combinedY)
+rfAccuracy = rfClassifier(X_train_std, y_train, combinedX, combinedY)
+dtAccuracy = dtClassifier(X_train_std, y_train, combinedX, combinedY)
 
-
-svmAccuracy = svm(X_train_std, y_train)
-
-# rfAccuracy = rfClassifier(X_train_std, y_train)
-# dtAccuracy = dtClassifier(X_train_std, y_train, combinedX, combinedY)
 # plotAccuracy(lrAccuracy, svmAccuracy, rfAccuracy, dtAccuracy)
 
